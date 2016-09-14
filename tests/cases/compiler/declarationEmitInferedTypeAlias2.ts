6b668c40f709909f2354f8f407f11a13cbeea831
Kanchalai Tanglertsampan <yuisu@microsoft.com>
2016-09-13 17:10:42 -0700

    Add tests

---
+++ tests/cases/compiler/declarationEmitInferedTypeAlias2.ts
@@ -0,0 +1,11 @@
+﻿// @declaration: true
+// @skipDefaultLibCheck: true
+
+// @Filename: 0.ts
+export type Data = string | boolean;
+let obj: Data = true;
+
+// @Filename: 1.ts
+import * as zero from "./0"
+let v = "str" || true;
+export { v }
